3925d04032a0df5c428bfe5da023e7b9180d2163
Jonah Warren <jonah@playfulsystems.com>
2021-02-24 10:25:56 -0500

    Updated movement mechanics.

---
+++ Assets/Scripts/RotateBothDirections.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBothDirections : MonoBehaviour
+{
+    public KeyCode keyClockwise;
+    public KeyCode keyCounterClockwise;
+    float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(keyClockwise))
+		{
+            GetComponent<Rigidbody2D>().rotation = GetComponent<Rigidbody2D>().rotation - speed;
+
+        }
+        else if (Input.GetKey(keyCounterClockwise))
+        {
+            GetComponent<Rigidbody2D>().rotation = GetComponent<Rigidbody2D>().rotation + speed;
+
+        }
+    }
+}
---
+++ Assets/Scripts/RotateOnKey.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnKey : MonoBehaviour
+{
+    public KeyCode key;
+    float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(key))
+		{
+            GetComponent<Rigidbody2D>().rotation = GetComponent<Rigidbody2D>().rotation + speed;
+
+        }
+    }
+}
